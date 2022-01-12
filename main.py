4a618bda32084421cf595f2c3d946c0f197a6916
naonmr <86028587+naonmr@users.noreply.github.com>
2022-01-12 14:41:32 +0900

    bugfix: test

--- main.py
+++ main.py
@@ -121,9 +121,7 @@ def handle_message(event):
             )
 
 
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+
 
 def send_message():
     line_bot_api.push_message(
@@ -139,8 +137,11 @@ def reminder():
 
 
 
+
+
 if __name__ == "__main__":
 #    app.run()
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+    reminder()
 
